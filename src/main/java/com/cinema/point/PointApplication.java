6a3568d7dd3df1230bc7d21830ef9d0a40933c0b
Rostyslaw <rostykzadyraichuk010@gmail.com>
2020-06-10 16:48:57 +0300

    pages add

--- src/main/java/com/cinema/point/PointApplication.java
+++ src/main/java/com/cinema/point/PointApplication.java
@@ -2,8 +2,14 @@ package com.cinema.point;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableGlobalMethodSecurity(
+		prePostEnabled = true,
+		securedEnabled = true,
+		jsr250Enabled = true)
 public class PointApplication {
 
 	public static void main(String[] args) {
